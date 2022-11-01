99d7823b87a179c359c3133d39f5125664156a68
serstuk <pc@pop-os.localdomain>
2022-11-01 10:27:52 +0100

    reqs added

--- Sol_BudikKivyMD.py
+++ Sol_BudikKivyMD.py
@@ -1,4 +1,6 @@
-<<<<<<< HEAD
+# kivy needs xsel or xclip installed in linux via apt get install 
+
+
 from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
 from kivy.app import App
@@ -57,7 +59,6 @@ class Solarny_Kalkukator(MDApp, CalcSol):
     def convert_R(self, args=0):
         if self.selecteddate:
             self.seldat = self.selecteddate
-            print("seldat")
         else:
             self.seldat = ""
             self.selecteddate = ""
@@ -75,7 +76,7 @@ class Solarny_Kalkukator(MDApp, CalcSol):
                     # self.label.text = location
                     return self.convert_R
                 else:
-                    print("ahoj")
+                    print("passed")
             else:
                 self.result_calc.text = ""
                 self.label.text = "Nesprávna hodnota"
@@ -108,7 +109,6 @@ class Solarny_Kalkukator(MDApp, CalcSol):
         self.toolbar = MDTopAppBar(title="Solárny kalkulátor")
         self.toolbar.pos_hint = {"top": 1}
         self.toolbar.right_action_items = [["rotate-3d-variant", lambda x: self.flip()]]
-
         screen.add_widget(self.toolbar)
 
         # logo
@@ -267,8 +267,8 @@ class Solarny_Kalkukator(MDApp, CalcSol):
 
     def on_save(self, instance, value, date_range):
         #  self.chosendate = value,
-        print(instance, value, date_range)
-        print(value)
+        # print(instance, value, date_range)
+        # print(value)
         self.selecteddate = value
         # return self.build()
 
@@ -276,310 +276,15 @@ class Solarny_Kalkukator(MDApp, CalcSol):
         """Events called when the "CANCEL" dialog box button is clicked."""
 
     def calendars(self, kv):
-        print("ahojj")
 
         return self.show_date_picker()
 
     def show_date_picker(self):
         Builder.load_string(KV)
         self.date_dialog = MDDatePicker()
-        print("kkt")
         self.date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
         self.date_dialog.open()
-        # print("ahoj",self.on_save)
-
-
-if __name__ == "__main__":
-    Solarny_Kalkukator().run()
-=======
-from kivymd.uix.screen import MDScreen
-from kivymd.app import MDApp
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.image import Image
-from kivymd.uix.button import MDFillRoundFlatIconButton, MDFillRoundFlatButton
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.label import MDLabel
-from kivymd.uix.toolbar import MDToolbar
-from kivymd.uix.pickers import MDDatePicker
-from Sol_budik import CalcSol
-from kivy.core.window import Window
-
-Window.minimum_height = 500
-Window.minimum_width = 500
-
-KV = """
-MDFloatLayout:
-
-    MDToolbar:
-        title: "MDDatePicker"
-        pos_hint: {"top": 1}
-        elevation: 10
-
-    MDRaisedButton:
-        text: "Open date picker"
-        pos_hint: {'center_x': .5, 'center_y': .5}
-        size_hint_y : 0.5
-        on_release: app.show_date_picker()
-"""
-# zatial nefungujuci slider
-KV2 = """
-Screen
-
-    MDSlider:
-        min: 0
-        max: 24
-        value: 12
-"""
-
-
-class Solarny_Budik(MDApp, CalcSol):
-    def flip(self):
-        self.result_calc.text = ""
-        self.label.text = ""
-        self.input.text = ""
-        self.vys1.text = ""
-        self.vys2.text = ""
-        self.vys3.text = ""
-        self.vys4.text = ""
-        self.vys5.text = ""
-        self.vys6.text = ""
-        self.vys7.text = ""
-        self.selecteddate = ""
-
-    def convert_R(self, args=0):
-        if self.selecteddate:
-            self.seldat = self.selecteddate
-            print("seldat")
-        else:
-            self.seldat = ""
-            self.selecteddate = ""
-        try:
-            val = str(self.input.text)
-            if len(val) > 3:
-                # CalcSol.UTCcl(self, val, self.seldat)
-                if CalcSol.UTCcl(self, val, self.seldat) != None:
-                    self.label.text = "Mesto neexistuje"
-                    return self.convert_R
-                resres = CalcSol.calculations(self, val)
-
-                if resres == None:
-                    self.label.text = "Nezadal si správnu adresu"
-                    # self.label.text = location
-                    return self.convert_R
-                else:
-                    print("ahoj")
-            else:
-                self.result_calc.text = ""
-                self.label.text = "Nesprávna hodnota"
-                self.input.text = ""
-                return self.convert_R
-        except ValueError:
-            self.result_calc.text = ""
-            self.label.text = "Nesprávna hodnota"
-            self.input.text = ""
-            return self.convert_R
-
-        self.result_calc.text = resres[20]
-        self.label.text = "Dnešný dátum je "
-
-        # podrobne vysl hodnoty
-        self.vys1.text = resres[0]
-        self.vys2.text = resres[17]
-        self.vys3.text = resres[18]
-        self.vys4.text = resres[16]
-        self.vys5.text = resres[1][0:5] + "°"
-        self.vys6.text = resres[2][0:5] + "°"
-        self.vys7.text = self.caz
-
-    def build(self):
-        screen = MDScreen()
-        Builder.load_string(KV2)
-        # top toolbar
-        self.selecteddate = ""
-        self.theme_cls.theme_style = "Dark"  # "Light"
-        self.toolbar = MDToolbar(title="Solárny budík")
-        self.toolbar.pos_hint = {"top": 1}
-        self.toolbar.right_action_items = [["rotate-3d-variant", lambda x: self.flip()]]
-
-        screen.add_widget(self.toolbar)
-
-        # logo
-        screen.add_widget(
-            Image(
-                source="sun-rise-1148031.jpg",
-                allow_stretch=True,
-                keep_ratio=False,
-                # height = 220, width = 220,
-                # pos=(100, 500),
-                # allow_stretch: True,
-                # size_hint_y: 0, # Tells the layout to ignore the size_hint in y dir
-                # size_hint=(1, 0.4),
-                size_hint_min=(500, 500),
-                size=(500, 1010),
-                pos_hint={"center_x": 0.5, "center_y": 0.4},
-            )
-        )
-        self.input = MDTextField(
-            text="",
-            hint_text="Zadaj svoju adresu ( mesto ) ",
-            halign="center",
-            size_hint=(0.8, 1),
-            pos_hint={"center_x": 0.5, "center_y": 0.80},
-            font_size=22,
-        )
-        screen.add_widget(self.input)
-        # label primanrny aj sekundarny
-        self.label = MDLabel(
-            halign="center",
-            pos_hint={"center_x": 0.5, "center_y": 0.75},
-            theme_text_color="Secondary",
-        )
-        self.result_calc = MDLabel(
-            halign="center",
-            pos_hint={"center_x": 0.5, "center_y": 0.70},
-            theme_text_color="Primary",
-            font_style="H5",
-        )
-        screen.add_widget(self.label)
-        screen.add_widget(self.result_calc)
-        # button convert
-        screen.add_widget(
-            MDFillRoundFlatButton(
-                text="Vypočítaj",
-                font_size=17,
-                pos_hint={"center_x": 0.5, "center_y": 0.65},
-                on_press=self.convert_R,
-            )
-        )
-        screen.add_widget(
-            MDFillRoundFlatButton(
-                text="Zadaj dátum",
-                font_size=17,
-                pos_hint={"center_x": 0.8, "center_y": 0.65},
-                on_press=self.calendars,
-            )
-        )
-        # podrobnosti vysledky
-        self.vys1 = MDLabel(
-            halign="left",
-            size_hint_min=(150, 200),
-            pos_hint={"center_x": 0.9, "center_y": 0.6},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.vys1)
-        self.vys2 = MDLabel(
-            halign="center",
-            pos_hint={"center_x": 0.45, "center_y": 0.55},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.vys2)
-        self.vys3 = MDLabel(
-            halign="center",
-            pos_hint={"center_x": 0.45, "center_y": 0.5},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.vys3)
-        self.vys4 = MDLabel(
-            halign="center",
-            pos_hint={"center_x": 0.45, "center_y": 0.45},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.vys4)
-        self.vys5 = MDLabel(
-            halign="center",
-            pos_hint={"center_x": 0.45, "center_y": 0.4},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.vys5)
-        self.vys6 = MDLabel(
-            halign="center",
-            pos_hint={"center_x": 0.45, "center_y": 0.35},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.vys6)
-
-        self.vys7 = MDLabel(
-            halign="center",
-            pos_hint={"center_x": 0.45, "center_y": 0.3},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.vys7)
-
-        self.inf1 = MDLabel(
-            text="Tvoja zadaná poloha",
-            halign="left",
-            pos_hint={"center_x": 0.6, "center_y": 0.60},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.inf1)
-        self.inf2 = MDLabel(
-            text="Čas východu slnka",
-            halign="left",
-            pos_hint={"center_x": 0.6, "center_y": 0.55},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.inf2)
-        self.inf3 = MDLabel(
-            text="Čas západu slnka",
-            halign="left",
-            pos_hint={"center_x": 0.6, "center_y": 0.5},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.inf3)
-        self.inf4 = MDLabel(
-            text="Poludnie",
-            halign="left",
-            pos_hint={"center_x": 0.6, "center_y": 0.45},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.inf4)
-        self.inf5 = MDLabel(
-            text="Zemepisná šírka",
-            halign="left",
-            pos_hint={"center_x": 0.6, "center_y": 0.4},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.inf5)
-        self.inf6 = MDLabel(
-            text="Zemepisná výška",
-            halign="left",
-            pos_hint={"center_x": 0.6, "center_y": 0.35},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.inf6)
-        self.inf7 = MDLabel(
-            text="Časové pásmo",
-            halign="left",
-            pos_hint={"center_x": 0.6, "center_y": 0.3},
-            theme_text_color="Secondary",
-        )
-        screen.add_widget(self.inf7)
-
-        return screen
-
-    def on_save(self, instance, value, date_range):
-        #  self.chosendate = value,
-        print(instance, value, date_range)
-        print(value)
-        self.selecteddate = value
-        # return self.build()
-
-    def on_cancel(self, instance, value):
-        """Events called when the "CANCEL" dialog box button is clicked."""
-
-    def calendars(self, kv):
-        print("ahojj")
-
-        return self.show_date_picker()
-
-    def show_date_picker(self):
-        Builder.load_string(KV)
-        self.date_dialog = MDDatePicker()
-        print("kkt")
-        self.date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
-        self.date_dialog.open()
-        # print("ahoj",self.on_save)
+        # print("save",self.on_save)
 
 
 if __name__ == "__main__":
--- Sol_budik.py
+++ Sol_budik.py
@@ -14,61 +14,30 @@ class CalcSol:
 
         geolocator = Nominatim(user_agent="anyName")
         tf = TimezoneFinder()
-        # location = geolocator.geocode(positionAA)
-        # print(location.address)
         self.coords = geolocator.geocode(positionAA)
-        print("coords", self.coords)
         if self.coords == None:
             resres = "Zadané mesto nenájdené"
             return resres
         self.timezone1 = tf.timezone_at(
             lng=self.coords.longitude, lat=self.coords.latitude
         )
-        print("ajooj", self.timezone1)
         naive = datetime.now()
         tmzn = pytz.timezone(self.timezone1)
-        print(tmzn, "tmzn")
         self.aware1 = naive.astimezone(tmzn)
-        print(self.aware1, "upraveny cas")
 
         cazon = re.split(r"(\D+)|(\D-)", str(self.aware1))
-        print("ojojo", selecteddate)
         if selecteddate != "":
-            # selecteddate =selecteddate.split("-")
-
-            print("seeeer", selecteddate)
             selecteddate = str(selecteddate)
-            print(type(selecteddate))
-            print(type(cazon))
             cazon[0] = str(selecteddate[0:4])
             cazon[3] = str(selecteddate[5:7])
             cazon[6] = str(selecteddate[8:])
         if selecteddate == "":
-            print("serss")
             cazon = re.split(r"(\D+)|(\D-)", str(self.aware1))
         self.caz1 = cazon
-        print(self.caz1)
-        print(cazon)
         cazon = cazon[-6] + cazon[-4] + cazon[-3] + cazon[-1]
-        print("cazon", cazon)
         self.caz = cazon
 
     def calculations(self, positionAA):
-        # x = strftime("%a, %d %b %Y %H:%M:%S")
-        # rok = int(strftime("%y"))
-        # print(type(rok))
-        # mesiac = int(strftime("%m"))
-        # den = int(strftime("%d"))
-        # den_v_roku = int(strftime("%j"))
-        # cas_zona = int(strftime("%z"))
-        # hodina = int(strftime("%H"))
-        # minuta =int(strftime("%M"))
-        # sekunda  = int(strftime("%S"))
-
-        # x = strftime("%a, %d %m %Y %H:%M:%S")
-        # print("X",x)
-        # print(type(x))
-        # print("CC",self.caz1)
         rok = int(self.caz1[0])
         mesiac = int(self.caz1[3])
         den = int(self.caz1[6])
@@ -78,35 +47,19 @@ class CalcSol:
         hodina = int(self.caz1[9])
         minuta = int(self.caz1[12])
         sekunda = int(self.caz1[15])
-        print(den)
-
-        print("cas zona", strftime("%z"))
 
         if strftime("%z")[0] == "+":
             cas_zona = int(strftime("%z")[1:3])
-            print("c1", cas_zona)
         else:
             cas_zona = -abs(int(strftime("%z")[1:3]))
-            print("c", cas_zona)
         self.tzzzz = float(cas_zona)
-        print(self.tzzzz, "tzzz")
-        print(type(self.tzzzz))
         self.tzzzz = self.caz1[19] + str(cas_zona)
-        print(self.tzzzz, "tzzz")
         self.tzzzz = int(self.tzzzz)
-        print(self.tzzzz, "tzzz")
-        print(rok, mesiac, den, self.tzzzz, den_v_roku, hodina)
         fr_rok = (2 * pi / 365) * (den_v_roku - 1 + (hodina - 12 / 24))
-        print("fr_rok", fr_rok)
         # cas= (strftime("%H:%M:%S"))
         # cas=(hodina+":"+minuta+":"+sekunda+":")
         cas = f"{hodina:02d}:{minuta:02d}:{sekunda:02d}"
-        print(cas[0:2], cas[3:5])
         cas_val = (int(cas[0:2]) * 60 + int(cas[3:5])) / 24 / 60
-        print("2")
-        print(cas_val, "Cas val")
-        print(cas)
-        print("cas", cas[0:2])
         geolocator = Nominatim(user_agent="Sol_budik")
         location = geolocator.geocode(positionAA)
         if location == None:
@@ -114,43 +67,20 @@ class CalcSol:
             return None
         else:
             print(location)
-        print(location.address)
-        print((location.latitude, location.longitude))
-        svet_sirka = location.longitude
         date_time_str = (
             f"{den:02d}/{mesiac:02d}/{rok} {hodina:02d}:{minuta:02d}:{sekunda:02d}"
         )
         date_time_str_sol = f"{hodina}:{minuta}:{sekunda}"
-        print(date_time_str)
-        print(type(date_time_str))
-        print(date_time_str_sol)
         date_time_obj = datetime.strptime(date_time_str, "%d/%m/%Y %H:%M:%S")
-        print("The type of the date is now", type(date_time_obj))
-        print(date_time_obj)
         date_time_sol = datetime.strptime(date_time_str_sol, "%H:%M:%S")
-
-        print(date_time_sol)
         time_delta_base = timedelta(
             hours=int(strftime("%H")),
             minutes=int(strftime("%M")),
             seconds=int(strftime("%S")),
         )
         time_delta_seconds = time_delta_base.total_seconds()
-        print("base", time_delta_base)
         time_delta_base = time_delta_base * 2
-        print("base2", time_delta_base)
-        print("1x", time_delta_seconds)
         time_delta_seconds = time_delta_seconds * 2
-        print("2x", time_delta_seconds)
-
-        date1 = JD = (
-            367 * rok
-            - 7 * (rok + (mesiac + 9) / 12) / 4
-            + 275 * mesiac / 9
-            + den
-            + 1721014
-        )
-        print(date1)
 
         def get_julian_datetime(date):
             """
@@ -183,41 +113,29 @@ class CalcSol:
                 - 0.5 * copysign(1, 100 * date.year + date.month - 190002.5)
                 + 0.5
             )
-
             return julian_datetime
 
         # example_datetime = datetime(2022, 3, 11, 7, 30, 0)
         example_datetime = date_time_obj
-        print(get_julian_datetime(example_datetime))
         julian_den = get_julian_datetime(example_datetime)
 
         jul_rok = (julian_den - 2451545) / 36525
-        print(jul_rok, "jul rok ")
-
         geomMeanLongSundeg = float(
             280.46646 + jul_rok * (36000.76983 + jul_rok * 0.0003032) % 360
         )
-        print("geomMeanlongSun OK ", geomMeanLongSundeg)
         geomMeanAnomSundeg = 357.52911 + jul_rok * (35999.05029 - 0.0001537 * jul_rok)
-        print("geomMeanAnomSun OK  ", geomMeanAnomSundeg)
         eccentEarthOrbit = 0.016708634 - jul_rok * (
             0.000042037 + 0.0000001267 * jul_rok
         )
-        print("eccentEarthOrbit OK", eccentEarthOrbit)
         sunEqofCtr = (
             sin(radians(geomMeanAnomSundeg))
             * (1.914602 - jul_rok * (0.004817 + 0.000014 * jul_rok))
             + sin(radians(2 * geomMeanAnomSundeg)) * (0.019993 - 0.000101 * jul_rok)
             + sin(radians(3 * geomMeanAnomSundeg)) * 0.000289
         )
-        print("sunEqofCtr OK", sunEqofCtr)
 
         sunTrueLongdeg = geomMeanLongSundeg + sunEqofCtr
-        print("sunTrueLongdeg OK ", sunTrueLongdeg)
         sunTrueAnomdeg = geomMeanAnomSundeg + sunEqofCtr
-        print("sunTrueAnomdeg OK ", sunTrueAnomdeg)
-        test_long = location.latitude
-        print(type(test_long))
         sun_rad_vector_AUs = (
             1.000001018 * (1 - eccentEarthOrbit * eccentEarthOrbit)
         ) / (1 + eccentEarthOrbit * cos(radians(sunTrueAnomdeg)))
@@ -249,6 +167,7 @@ class CalcSol:
                 cos(radians(sun_app_long_deg)),
             )
         )
+        """
         print(
             "!!!!!!!!",
             degrees(
@@ -271,6 +190,8 @@ class CalcSol:
                 )
             )
         )
+        """
+
         sun_declin_deg = degrees(
             asin(sin(radians(obliq_corr_deg)) * sin(radians(sun_app_long_deg)))
         )
@@ -296,8 +217,7 @@ class CalcSol:
                 - tan(radians(location.latitude)) * tan(radians(sun_declin_deg))
             )
         )
-        print(ha_sunrise_deg, "ha_sunrise_deg")
-        print(
+        """print(
             sun_rad_vector_AUs,
             "sun_rad_vector_AUs OK ",
             sun_app_long_deg,
@@ -305,7 +225,7 @@ class CalcSol:
             mean_obliq_ecliptic_deg,
             "mean_obliq_ecliptic_deg OK ",
         )
-        print(
+         print(
             obliq_corr_deg,
             "obliq_corr_deg",
             sun_rt_ascen_deg,
@@ -317,22 +237,16 @@ class CalcSol:
             eq_of_time_minutes,
             "eq_of_time_minutes  ",
         )
+        """
         solar_noon_LST = (
             720 - 4 * location.longitude - eq_of_time_minutes + self.tzzzz * 60
         ) / 1440
-        print(solar_noon_LST, "solar_noon_LST")
         sunrise_time_LST = solar_noon_LST - ha_sunrise_deg * 4 / 1440
-        print(sunrise_time_LST, "Sunrise")
         sunset_time_LST = solar_noon_LST + ha_sunrise_deg * 4 / 1440
-        print(sunset_time_LST, "sunset_time_LST")
         sunlight_duration_minutes = 8 * ha_sunrise_deg
-        print(sunlight_duration_minutes, "sunlight_duration_minutes")
-        print("eq", eq_of_time_minutes)
-        print(type(cas))
         true_solar_time_min = (
             cas_val * 1440 + eq_of_time_minutes + 4 * location.longitude - 60 * 1
         ) % 1440
-        print(true_solar_time_min, "true_solar_time_min")
         if true_solar_time_min / 4 < 0:
             hour_angle_deg = true_solar_time_min / 4 + 180
         else:
@@ -423,12 +337,8 @@ class CalcSol:
         # MOD(540-DEGREES(ACOS(((SIN(RADIANS($B$3))*COS(RADIANS(AD2)))-SIN(RADIANS(T2)))/(COS(RADIANS($B$3))*SIN(RADIANS(AD2)))));360))
 
         sunrise_time_LST = str(timedelta(days=sunrise_time_LST, seconds=0))[0:9]
-        print("split", sunrise_time_LST)
-        print(type(sunrise_time_LST))
         sunrise_time_LST = sunrise_time_LST.split(":", maxsplit=2)
         sunrise_time_LST = ":".join(sunrise_time_LST[0:2])
-        print("split", sunrise_time_LST)
-        print(type(sunrise_time_LST))
         sunset_time_LST = str(timedelta(days=sunset_time_LST, seconds=0))[0:9]
         sunset_time_LST = sunset_time_LST.split(":", maxsplit=2)
         sunset_time_LST = ":".join(sunset_time_LST[0:2])
@@ -436,7 +346,6 @@ class CalcSol:
         solar_noon_LST = solar_noon_LST.split(":", maxsplit=2)
         solar_noon_LST = ":".join(solar_noon_LST[0:2])
 
-        rerere = str("sabinooov")
         resres = [
             location.address,
             location.latitude,
@@ -461,12 +370,7 @@ class CalcSol:
             date_time_str,
         ]
         resres = [str(x) for x in resres]
-        print(len(resres))
         return resres
-
-        # str(location)
-        # "tu som ",
-        #  type(str(location)),
 
 
 d = CalcSol()
