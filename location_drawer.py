eaf9114356826cba4c7af93b14819903890c9448
serstuk <radoslav993@gmail.com>
2022-11-07 23:25:02 +0100

    transparency bug fix

--- location_drawer.py
+++ location_drawer.py
@@ -19,15 +19,22 @@ class MapLocator:
         self.latt = latt
 
     def image_merger(self):  # merge 2 images with scale
-        self.pin_img = Image.open("pin.png", mode="r", formats=None)
+        self.pin_img = Image.open("p5.png", mode="r", formats=None).convert('RGBA')
         size = self.pin_img.size
-        # print(size)
+        #print(size)
         self.pin_img = ImageOps.fit(
             self.pin_img, (25, 25), method=0, bleed=0.0, centering=(0.0, 0.0)
-        )
+        ).convert('RGBA')
         size = (1000, 500)
+        self.temp_img = Image.new("RGBA", size, (0, 0, 0, 0))
+       # temp_img.show() 
+        self.temp_img.paste(self.pin_img, self.pixel_coords())
+
+    
         self.image = Image.open("temp.jpg", mode="r", formats=None)
-        self.image = ImageOps.fit(self.image, size, bleed=0.0, centering=(0.5, 0.5))
+        self.image = ImageOps.fit(self.image, size, bleed=0.0, centering=(0.5, 0.5)).convert('RGBA')
+
+
 
     def pixel_coords(
         self,
@@ -46,45 +53,22 @@ class MapLocator:
 
         y, x = 49 * -1 + 84, 0
         y, x = float(self.latt) * -1 + 84, float(self.lng)
-        long = (int(x % 360 / 360 * 1000 + 500) - 12) % 1000
-        lat = (int(((y % 170) / 170) * 500) - 12) % 500
+        long = int((x % 360 / 360 * 1000 + 500) - 12.5) % 1000
+        lat = int(((((y % 170) / 170) * 500) - 12.5) % 500)
         locat = (long, lat)
-        # print(locat)
+        #print(locat)
         return locat
 
     def img_map_generator(self):  # merge 2 images and return location
 
         self.image_merger()
-        self.imgl = "t1.jpg"
-        self.image.paste(self.pin_img, self.pixel_coords())
-        self.image = self.image.save(self.imgl)
-
-        """   # create memory location of image without physical storing it 
-        import io
-        # BytesIO is a fake file stored in memory
-        imgByteArr = io.BytesIO()
-        # image.save expects a file as a argument, passing a bytes io ins
-        self.image.save(imgByteArr, format='PNG')
-        # Turn the BytesIO object back into a bytes object
-        imgByteArr = imgByteArr.getvalue()
-        ss = Image.open(io.BytesIO(imgByteArr))
-        print("SS",ss)
-        # ss.show()
-        """
-
-        return "t1.jpg"
-
-        """ # create new image for testing 
-        image = Image.new("RGB", (300, 50))
-        draw = ImageDraw.Draw(image)
-        draw.text((0, 0), "This text is drawn on image")
-        byte_io = BytesIO()
-        image.save(byte_io, 'PNG')
-        image.show()
-        """
+        self.imgl = "t1.png"
+        m_img = Image.alpha_composite(self.image, self.temp_img)
+        #m_img.show()
+        m_img = m_img.save(self.imgl)
+        return "t1.png"
 
 
 if __name__ == "__main__":
-    print("A")
-    d = MapLocator(35, 140)
+    d = MapLocator(35, 139.5)
     d.img_map_generator()
